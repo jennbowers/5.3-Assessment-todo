bf00bcb76953ff7a05c1718f27e4587968b4e912
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-21 19:17:58 -0400

    finished 5.3 Assessment

--- app.js
+++ app.js
@@ -14,29 +14,49 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const context = {
+var todoIdx = 0;
+var completedIdx = 0;
+
+var context = {
   todoList: [
-    {task: 'Feed dog'
-    , done: ''}
-    , {task: 'Mow the lawn'
-    , done: ''}
-    , {task: 'Do your homework'
-    , done: ''}
-]
+    'Feed dog'
+    , 'Mow the lawn'
+    , 'Do your homework'
+  ]
+  , todoId: function(){
+    return todoIdx++;
+  }
+  , completed: []
+  , completedId: function(){
+    return completedIdx++;
+  }
+
 };
 
-var todo = context.todoList;
 
 app.get('/', function(req, res) {
+  todoIdx = 0;
   res.render('index', context);
 });
 
 
 app.post('/', function(req, res) {
-  todo.push({task: req.body.todo_enter, done: ''});
+  var todo = context.todoList;
+  todo.push(req.body.todo_enter);
   console.log(todo)
-  res.render('index', context);
+  res.redirect('/');
 });
+
+
+app.post('/todo/:id', function(req, res) {
+  console.log('working');
+  var id = req.params.id;
+  // var completed_item = context.todo[id];
+  var todo_removed = context.todoList.splice(id, 1);
+  context.completed.push(todo_removed);
+  res.redirect('/');
+});
+
 
 
 // example given in assignment
---
+++ public/main.js
@@ -0,0 +1,8 @@
+// var todoCheckboxes = document.querySelectorAll('.todo_item');
+// for (var = i; i < todoCheckboxes.length; i++) {
+//   var input = todoCheckboxes[i];
+//   input.addEventListener('click', function(event) {
+//     var checkbox = event.target;
+//     checkbox.parentNode.submit();
+//   });
+// }

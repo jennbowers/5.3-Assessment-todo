eb309a684b530814dc878ca7b9d0321bee9b1cf3
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-21 15:37:49 -0400

    added boilerplate to index.mustache and rendered mustache page

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+
+app.get('/', function(req, res) {
+  res.render('index', {});
+});
+
+
 app.listen(3000, function() {
   console.log('Successfully started express application!');
 });

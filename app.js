c1015e57b3384a94fd07aa9ed9cde40415a82080
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-21 15:30:53 -0400

    added boilerplate to app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!');
+});
